3aea0f54a6d02b42a3cc69e42f911f015c5ae6f7
Patrick Banta <pbanta@gmail.com>
2025-12-29 14:52:36 -0600

    Use Berkeley Mono font, remove AnsiHeadshot component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
   }
